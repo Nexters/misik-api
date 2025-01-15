246bb5a824d7b23043f0de48f3cff4ef0f602720
xb205 <62425964+devxb@users.noreply.github.com>
2025-01-15 21:29:09 +0900

    feat: Setting project denpendency, CI, github projects (#2)
    * feat: setting project dependencies
    
    * ci: Integration test를 추가한다
    
    * feat: pr 템플릿을 정의한다

---
+++ src/main/kotlin/com/misik/api/Application.kt
@@ -0,0 +1,22 @@
+package com.misik.api
+
+import org.rooftop.netx.meta.EnableSaga
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.retry.annotation.EnableRetry
+
+@EnableSaga
+@EnableRetry
+@EnableJpaAuditing
+@SpringBootApplication
+class Application {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+}
