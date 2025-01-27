79ede9690409a2ad4355ebc86af7a4649484978e
devxb <develxb@gmail.com>
2025-01-27 15:56:27 +0900

    feat: Review조회 기능을 개발한다

--- src/main/kotlin/me/misik/api/api/ReviewController.kt
+++ src/main/kotlin/me/misik/api/api/ReviewController.kt
@@ -1,17 +1,18 @@
 package me.misik.api.api
 
+import me.misik.api.api.response.ReviewResponse
 import me.misik.api.domain.request.CreateReviewRequest
 import me.misik.api.app.CreateReviewFacade
+import me.misik.api.app.GetReviewFacade
 import me.misik.api.app.ReCreateReviewFacade
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ReviewController(
     private val createReviewFacade: CreateReviewFacade,
+    private val getReviewFacade: GetReviewFacade,
     private val reCreateReviewFacade: ReCreateReviewFacade,
 ) {
 
@@ -26,4 +27,9 @@ class ReviewController(
         @RequestHeader("device-id") deviceId: String,
         @PathVariable("id") id: Long,
     ) = reCreateReviewFacade.reCreateReviewInBackground(deviceId, id)
+
+    @GetMapping("reviews/{id}")
+    fun getReview(
+        @PathVariable("id") id: Long,
+    ): ReviewResponse = ReviewResponse.of(getReviewFacade.getReview(id))
 }
