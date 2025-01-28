72852bde2d23e26235400185b4c287c6fccac521
inpink <inpink@kakao.com>
2025-01-29 04:26:10 +0900

    refactor: 사용하지 않는 코드 제거

--- src/main/kotlin/me/misik/api/core/Chatbot.kt
+++ src/main/kotlin/me/misik/api/core/Chatbot.kt
@@ -36,41 +36,12 @@ fun interface Chatbot {
         }
 
         companion object {
-            val cachedParsingSystemMessage = Message.createSystem(
-                """
-                    리뷰에 쓸만한 정보를 추출해줘. key에는 방문 장소명, 품명 등이 포함될 수 있어. key는 최대 3개만 뽑아줘. 
-                    응답 형식은 반드시 다음과 같은 JSON이야. 응답에는 해당 JSON만 있어야해. 
-                    {
-                      "parsed": [
-                        {
-                          "key": "품명",
-                          "value": "카야토스트+음료세트"
-                        },
-                        {
-                          "key": "가격",
-                          "value": "카야토스트+음료세트"
-                        },
-                        ...
-                      ]
-                    }
-                   응답의 총 길이는 300자를 넘으면 안돼.
-                """
-            )
 
             fun from(review: Review): Request {
                 return Request(
                     messages = listOf(
                         Message.createSystem(review.requestPrompt.promptCommand + review.requestPrompt.hashTags.joinToString(", ") { "$it" }),
                         Message.createUser(review.requestPrompt.ocrText)
-                    )
-                )
-            }
-
-            fun createParsedOcr(ocrText: String): Request {
-                return Request(
-                    messages = listOf(
-                        cachedParsingSystemMessage,
-                        Message.createUser(ocrText)
                     )
                 )
             }
