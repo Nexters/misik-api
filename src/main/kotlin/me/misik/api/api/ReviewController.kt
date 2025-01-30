366958d19683324a21e25f1602e5463cd4eab453
inpink <108166692+inpink@users.noreply.github.com>
2025-01-31 00:42:51 +0900

    feat: 선택 가능한 어조를 조회한다 (#14)
    * feat: 선택 가능한 어조를 조회한다
    
    * refactor: Enum 요소를 가져오는 방법을 수정한다

--- src/main/kotlin/me/misik/api/api/ReviewController.kt
+++ src/main/kotlin/me/misik/api/api/ReviewController.kt
@@ -1,8 +1,12 @@
 package me.misik.api.api
 
-import me.misik.api.domain.request.CreateReviewRequest
+import me.misik.api.api.request.CreateReviewRequest
+import me.misik.api.api.response.ReviewStyleResponse
+import me.misik.api.api.response.ReviewStylesResponse
 import me.misik.api.app.CreateReviewFacade
 import me.misik.api.app.ReCreateReviewFacade
+import me.misik.api.domain.ReviewStyle
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -12,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class ReviewController(
     private val createReviewFacade: CreateReviewFacade,
-    private val reCreateReviewFacade: ReCreateReviewFacade,
+    private val reCreateReviewFacade: ReCreateReviewFacade
 ) {
 
     @PostMapping("reviews")
@@ -26,4 +30,11 @@ class ReviewController(
         @RequestHeader("device-id") deviceId: String,
         @PathVariable("id") id: Long,
     ) = reCreateReviewFacade.reCreateReviewInBackground(deviceId, id)
+
+    @GetMapping("reviews/styles")
+    fun getReviewStyles() : ReviewStylesResponse {
+        val reviewStyles = ReviewStyle.entries.toList()
+
+        return ReviewStylesResponse.from(reviewStyles)
+    }
 }
