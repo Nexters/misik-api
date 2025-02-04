a9c353c6cf81f108c7f50aa5b99e51d456d74713
inpink <inpink@kakao.com>
2025-02-04 21:54:29 +0900

    feat: 선택 가능한 어조를 조회한다 (#14)
    * feat: 선택 가능한 어조를 조회한다
    
    * refactor: Enum 요소를 가져오는 방법을 수정한다

--- src/main/kotlin/me/misik/api/api/ReviewController.kt
+++ src/main/kotlin/me/misik/api/api/ReviewController.kt
@@ -3,6 +3,9 @@ package me.misik.api.api
 import me.misik.api.domain.request.CreateReviewRequest
 import me.misik.api.app.CreateReviewFacade
 import me.misik.api.app.ReCreateReviewFacade
+import me.misik.api.domain.ReviewStyle
+import me.misik.api.domain.response.ReviewStylesResponse
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -26,4 +29,11 @@ class ReviewController(
         @RequestHeader("device-id") deviceId: String,
         @PathVariable("id") id: Long,
     ) = reCreateReviewFacade.reCreateReviewInBackground(deviceId, id)
+
+    @GetMapping("reviews/styles")
+    fun getReviewStyles() : ReviewStylesResponse {
+        val reviewStyles = ReviewStyle.entries.toList()
+
+        return ReviewStylesResponse.from(reviewStyles)
+    }
 }
