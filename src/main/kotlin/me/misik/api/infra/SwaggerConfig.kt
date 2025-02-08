c682b99b11abd25ef06c91c61b29b137bfaac24f
xb205 <62425964+devxb@users.noreply.github.com>
2025-02-08 16:04:09 +0900

    openapi 버전 명시 (#31)
    * openapi 버전을 명시한다
    
    * fix: merge
    
    * fix: Swagger config를 수정한다

--- src/main/kotlin/me/misik/api/infra/SwaggerConfig.kt
+++ src/main/kotlin/me/misik/api/infra/SwaggerConfig.kt
@@ -2,6 +2,7 @@ package me.misik.api.infra
 
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -10,10 +11,20 @@ class SwaggerConfig {
 
     @Bean
     fun customOpenAPI(): OpenAPI {
+        val info = Info()
+            .title("Misik API")
+            .version("1.0")
+            .description("Misik API Description")
+
+        val server = Server()
+        server.url = SWAGGER_URL
+
         return OpenAPI()
-            .info(
-                Info().title("Misik API")
-                .version("1.0")
-                .description("Misik API Description"))
+            .info(info)
+            .servers(listOf(server))
+    }
+
+    private companion object {
+        private const val SWAGGER_URL = "misik-api"
     }
 }
