513b89792b7625cc785d22a171ded51edd3edc15
inpink <108166692+inpink@users.noreply.github.com>
2025-02-08 17:21:30 +0900

    feat: Swagger를 추가한다 (#33)
    * feat: Swagger를 추가한다
    
    * feat: Swagger path를 설정한다
    
    * feat: 충돌 해소
    
    * feat: springdoc 버전 업그레이드

--- src/main/kotlin/me/misik/api/infra/SwaggerConfig.kt
+++ src/main/kotlin/me/misik/api/infra/SwaggerConfig.kt
@@ -2,7 +2,6 @@ package me.misik.api.infra
 
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -11,20 +10,10 @@ class SwaggerConfig {
 
     @Bean
     fun customOpenAPI(): OpenAPI {
-        val info = Info()
-            .title("Misik API")
-            .version("1.0")
-            .description("Misik API Description")
-
-        val server = Server()
-        server.url = SWAGGER_URL
-
         return OpenAPI()
-            .info(info)
-            .servers(listOf(server))
-    }
-
-    private companion object {
-        private const val SWAGGER_URL = "misik-api"
+            .info(
+                Info().title("Misik API")
+                .version("1.0")
+                .description("Misik API Description"))
     }
 }
